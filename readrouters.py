66ab10a782280e497000768b52d3fd6f2f967eb1
Quang <quang.m.nguyen@wsu.edu>
2015-11-11 21:06:38 -0800

    setupscript testdirname [-p option] - Check     - call readrouters - check     - call DVsimulator for each router - check
    DVsimulator [-p option] testdirname routername
        - create output file (ex: routername.output)                    - check
        - call readrouters to grab address and port to setup sockets    - sorta?
        - call readlink based on the routername                         - check
           (ex: DVtable = readlinks(testdirname, routername) )
            - grab DVtable info stored as a dictionary                  - check

--- readrouters.py
+++ readrouters.py
@@ -1,4 +1,6 @@
 import string
+import socket
+import sys
 class RouterInfo:
     def __init__(self, host, baseport):
         self.host, self.baseport = host, baseport;
@@ -13,10 +15,71 @@ def readrouters(testname):
     table = {}
     for line in lines:
         if line[0]=='#': continue
-        words = string.split(line)
+        #print line
+        words = line.split(" ")    
         table[words[0]] = RouterInfo(words[1], int(words[2]))
+
     f.close()
+    for x in table:
+        print "router info: "
+        attrs = vars(table[x])
+        print ', '.join("%s: %s" % item for item in attrs.items())
+             
     return table
+def dvsimulator(poption, testdirame, routerame, table):
+    dvtable = {}
+    print '----ENTERING dvsimulator-----'
+    print "poption : "+ poption
+    print "testdirame : "+ testdirame
+    print "routerame : "+ routerame
+
+
+    for x in table:
+        print x
+        attrs = vars(table[x])
+        print 'baseport:'
+        print attrs['baseport']
+        #putting readlink table to the dv table
+        dvtable[x[0]] = readlinks(testdirame, x[0])
+        #creating output file
+        f = open(x[0] + ".output",'w')
+        f.close()
+
+        makelisten(routerame,attrs['baseport'])
+        for q in dvtable:
+            print dvtable[q[0]]
+            #I ENDED HERE!
+
+
+        #attrs = vars(table[x])
+        #print ', '.join("%s: %s" % item for item in attrs.items())
+
+def makelisten(host, port):
+    print '----ENTERING MAKELISTEN Script-----'
+    print 'host:'
+    print host
+    print 'port'
+    print port
+    try:
+        #create an AF_INET, STREAM socket (TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error, msg:
+        print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+        sys.exit();
+     
+    print 'Socket Created'
+
+    try:
+        s.bind(("127.0.0.1",port))
+    except socket.error , msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+         
+    print 'Socket bind complete'
+    s.listen(10)
+    print 'Socket now listening'
+
+    #not sure what else to do as we didn't know how to listen or talk ect...
 
 def readlinks(testname, router):
     f = open(testname+'/'+router+'.cfg')
@@ -29,3 +92,18 @@ def readlinks(testname, router):
     f.close()
     return table
     
+
+
+def setupscript(testdirname, poption):
+    print '----ENTERING Set-Up Script-----'
+    table = {}
+    table = readrouters(testdirname)
+    for r in table:
+        #print table['A']
+        dvsimulator(poption,testdirname,r[0], table)
+
+
+readrouters('test1')
+#dvsimulator('hey','hey','hey')
+setupscript('test1', '-d');
+
