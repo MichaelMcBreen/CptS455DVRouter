a6d626fa84432fd043d38094ef8ef8002d6b256b
benjamin-cheung <cheungbenjamin@rocketmail.com>
2015-11-13 15:44:33 -0800

    select loop handles multiple messages

--- selectLoop.py
+++ selectLoop.py
@@ -2,6 +2,7 @@
 import select
 import string
 import time
+import re
 
 # make the set empty
 def FD_ZERO(setFDs):
@@ -20,32 +21,54 @@ def FD_CLR (fd, setFDs):
 def FD_ISSET (fd, setFDs):
     return fd in setFDs? 1 : 0
 
+def readAll(sock):
+	data = []
+	while True:
+		chunk = sock.recv(2048)
+		if not chunk: break
+		data.append(chunk)
+	return ''.join(data)
 
-# assuming we have servSock[], maxDescPlus1, numSockets
+def msgSplit(data):
+	messages = []
+	j = 0
+	i = 0
+	while i < len(data):
+		j = i + 1
+		while j < len(data) and not (j == len(data) or str(data[j]) == 'U' or str(data[j]) == 'L' or data[j:j+1] == 'P'):
+			j = j + 1
+		messages.append(data[i:j].strip(' '))
+		i = j
+	return messages
+
+# map read socket fds to read socket names in a dictionary
+# assuming we have servSock[]
+
 while 1:
 	start = time.time()
+	
 	rReady, wReady, eReady = select.select(readFDs, writeFDs, exceptFDs, 0);
 
+	# error handling
 	if len(rReady) < 0:
 		print "Select Error: failed to select available sockets\n"	# error handling????
+	# timeout handling
 	elif len(rReady) == 0:
 		print "Timeout Error: No available sockets"
+	# read available messages
 	else:
 		for fd in rReady:
-			# do something with serSock[fd]
-			data = servSock[fd].recv(2048)
-			updaterouterDVtable(data)
-	
+			# read all data in socket and parse messages
+			data = readAll(servSock[fd][0])
+			messages = msgSplit(data)
+			for m in messages:
+				DVUpdateMessage(servSock[fd][1], m)
+	# send updated DVtable to all available neighbors
 	for fd in writeFDs:
-		servSock[fd].send(data)
+		servSock[fd][0].send(data)
 	
 	end = time.time()
+	# updates sent every 30 seconds
 	time.sleep(30 - end + start)
-
-
-
-
-
-
 
 
